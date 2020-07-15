273c52b699ceda4ce8f065ae7daf48c44527ff25
Justin Hugon <jhugon@users.noreply.github.com>
2020-07-14 20:32:51 -0500

    ascii_serial_com_dummy_loopback_device: fleshed out loopback
      not tested
      doesn't yet involve ascii serial com

--- flags.mk
+++ flags.mk
@@ -3,13 +3,13 @@
 #$(info build_type is ${build_type})
 
 ifneq (,$(findstring gcc,$(CC)))
-  GCCFLAGS=-Werror -pedantic -pedantic-errors -Wall -Wextra
+  GCCFLAGS=-Werror -Wall -Wextra
   GCCFLAGS+=-Wdouble-promotion -Wformat=2 -Wformat-signedness -Winit-self -Wmissing-include-dirs -Wswitch-default -Wswitch-enum -Wuninitialized -Wtrampolines -Wfloat-equal -Wshadow -Wundef -Wbad-function-cast
   CFLAGS=$(GCCFLAGS)
-  CXXFLAGS=$(GCCFLAGS) -std=c++17 -Wsuggest-override -Wplacement-new=2
+  CXXFLAGS=$(GCCFLAGS) -std=gnu++17 -Wsuggest-override -Wplacement-new=2
   ifneq (,$(findstring avr,$(CC)))
   else
-    CFLAGS+= -std=c18 -Wformat-overflow=2 -Wformat-truncation=2 -Wnull-dereference -Walloc-zero -Wduplicated-branches -Wduplicated-cond
+    CFLAGS+= -std=gnu18 -Wformat-overflow=2 -Wformat-truncation=2 -Wnull-dereference -Walloc-zero -Wduplicated-branches -Wduplicated-cond
   endif
   ifeq ($(build_type),debug)
     CFLAGS+=-g -Og
@@ -24,10 +24,10 @@ ifneq (,$(findstring gcc,$(CC)))
 endif
 
 ifneq (,$(findstring clang,$(CC)))
-  CLANGFLAGS=-Werror -pedantic -pedantic-errors -Wall -Wextra
+  CLANGFLAGS=-Werror -Wall -Wextra
   CLANGFLAGS+=-Wdouble-promotion -Winit-self -Wmissing-include-dirs -Wswitch-default -Wswitch-enum -Wuninitialized -Wfloat-equal -Wshadow -Wundef -Wbad-function-cast
-  CFLAGS=$(CLANGFLAGS) -std=c18
-  CXXFLAGS=$(CLANGFLAGS) -std=c++14 -Wsuggest-override -Wplacement-new=2
+  CFLAGS=$(CLANGFLAGS) -std=gnu18
+  CXXFLAGS=$(CLANGFLAGS) -std=gnu++14 -Wsuggest-override -Wplacement-new=2
   ifeq ($(build_type),debug)
     CFLAGS+=-g -Og -fsanitize=address -fsanitize=leak -fsanitize=undefined -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fsanitize-address-use-after-scope -fstack-protector-all #-fsanitize=pointer-compare -fsanitize=pointer-subtract
   else
--- src/ascii_serial_com_dummy_loopback_device.c
+++ src/ascii_serial_com_dummy_loopback_device.c
@@ -1,6 +1,13 @@
+#include <poll.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "ascii_serial_com.h"
+#include "circular_buffer.h"
+
+#define bufCap 64
+circular_buffer_uint8 buffer;
+uint8_t buffer_raw[bufCap];
 
 int main(int argc, char *argv[]) {
 
@@ -12,5 +19,94 @@ int main(int argc, char *argv[]) {
   const char *infilename = argv[1];
   const char *outfilename = argv[2];
   printf("infile: %s\noutfile: %s\n", infilename, outfilename);
+
+  FILE *infile = fopen(infilename, "r");
+  if (!infile) {
+    perror("Error opening input file");
+    printf("Exiting.\n");
+    return 1;
+  }
+  int infileno = fileno(infile);
+  if (infileno < 0) {
+    perror("Error getting infile descriptor");
+    printf("Exiting.\n");
+    return 1;
+  }
+
+  FILE *outfile = fopen(outfilename, "a+");
+  if (!outfile) {
+    perror("Error opening output file");
+    printf("Exiting.\n");
+    return 1;
+  }
+  int outfileno = fileno(outfile);
+  if (outfileno < 0) {
+    perror("Error getting infile descriptor");
+    printf("Exiting.\n");
+    return 1;
+  }
+
+  struct pollfd fds[2] = {
+      {infileno, POLLIN | POLLHUP, 0},
+      {outfileno, POLLOUT | POLLHUP, 0},
+  };
+  short *inflags = &fds[0].revents;
+  short *outflags = &fds[0].revents;
+
+  circular_buffer_init_uint8(&buffer, bufCap, buffer_raw);
+
+  while (true) {
+    int ready = poll(fds, 2, -1);
+    if (ready < 0) {
+      perror("Error while polling");
+      printf("Exiting.\n");
+      return 1;
+    }
+    if ((*inflags & POLLERR) > 0) {
+      printf("Infile error, exiting.\n");
+    }
+    if ((*outflags & POLLERR) > 0) {
+      printf("Outfile error, exiting.\n");
+    }
+    if ((*inflags & POLLHUP) > 0) {
+      printf("Infile hung up, exiting.\n");
+    }
+    if ((*outflags & POLLHUP) > 0) {
+      printf("Outfile hung up, exiting.\n");
+    }
+    if ((*inflags & POLLNVAL) > 0) {
+      printf("Infile closed, exiting.\n");
+    }
+    if ((*outflags & POLLNVAL) > 0) {
+      printf("Outfile closed, exiting.\n");
+    }
+    if ((*inflags & POLLIN) > 0) {
+      // read
+      if (circular_buffer_is_full_uint8(&buffer)) {
+        if (!usleep(1000)) {
+          perror("Error while sleeping waiting for read");
+          printf("Exiting.\n");
+          return 1;
+        }
+      } else {
+        const uint8_t c = fgetc(infile);
+        circular_buffer_push_back_uint8(&buffer, c);
+      }
+    }
+    if ((*outflags & POLLOUT) > 0) {
+      // write
+      if (circular_buffer_is_empty_uint8(&buffer)) {
+        if (!usleep(1000)) {
+          perror("Error while sleeping waiting for read");
+          printf("Exiting.\n");
+          return 1;
+        }
+      } else {
+        const uint8_t c = circular_buffer_pop_front_uint8(&buffer);
+        fputc(c, outfile);
+      }
+    }
+  }
+
   return 0;
 }
