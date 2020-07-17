e80f925f2207f405d59922eecb3f65af0d727ff8
Justin Hugon <jhugon@users.noreply.github.com>
2020-07-17 14:02:02 -0500

    Working on plans for real devices
      modified README.md and src/ascii_serial_com_device.h

--- README.md
+++ README.md
@@ -3,3 +3,41 @@
 Serial communication library between computers and microcontrollers, FPGAs,
 etc. Uses only ASCII. Not the most efficient protocol, but meant to be easy to
 read.
+
+## Transmit/Receive Data Interface
+
+Will need to be able to transmit and receive messages. This could mean just
+putting them in the input and output buffers within ASCII-Serial-Com, and then
+having some other functions to actually transmit or receive the bytes from
+those buffers into other buffers etc.
+
+May need a polling loop or even software interrupt that says the receive buffer
+needs to be parsed for a message.
+
+### AVR
+
+For transmission: a flag or interrupt will say the (single byte) transmit
+buffer is empty. Will need to pop a byte off of the ASCII-Serial-Com output
+buffer to put in there. Could also have another buffer in between.
+
+For reception: a flag or interrupt will say the (two byte on 328P) receive
+buffer is not empty. Will need to push a byte into the ASCII-Serial-Com input
+buffer. Could also have another buffer in between
+
+### STM32
+
+(Need to understand DMA better)
+
+Can do similar thing to AVR or use DMA:
+
+An interrupt will say a DMA buffer is empty/half-full/full. Will need to
+pop/push from/to those buffers as appropriate. There may need to be an
+abstraction around the DMA buffers to help out.
+
+### Linux Native
+
+Will involve fread or read and fwrite or write. Maybe poll or select.
+
+Maybe poll flags are set on the input and output streams depending on the state
+of the ASCII-Serial-Com buffers. Would then need to check if the receive buffer
+needs to be parsed after input reads.
--- src/ascii_serial_com_device.h
+++ src/ascii_serial_com_device.h
@@ -4,12 +4,12 @@
 
 #include "ascii_serial_com.h"
 
-/** \brief ASCII Serial Com Interface State struct
+/** \brief ASCII Serial Com Device State struct
  *
- *  Keeps track of the state of the ASCII Serial Com interface
+ *  Keeps track of the state of the ASCII Serial Com device
  *
  *  The functions take the stuff from ascii_serial_com_receive plus a void
- * pointer to possible state/configuration info If the functions are null ptrs,
+ * pointer to possible state/configuration info. If the functions are null ptrs,
  * a check will be done and error message returned to host
  */
 typedef struct ascii_serial_com_device_struct {
@@ -22,3 +22,31 @@ typedef struct ascii_serial_com_device_struct {
   void (*fother)(ascii_serial_com *, char, char, char, const char *data, size_t,
                  void *); /**< called for other messages */
 } ascii_serial_com_device;
+
+/** \brief ASCII Serial Com Device init
+ *
+ * Initialize ASCII Serial Com device
+ *
+ *  The functions take the stuff from ascii_serial_com_receive plus a void
+ * pointer to possible state/configuration info. If the functions are null ptrs,
+ * a check will be done and error message returned to host
+ *
+ */
+void ascii_serial_com_device_init(
+    void (*frw)(ascii_serial_com *, char, char, char, const char *data, size_t,
+                void *); /**< called for r or w messages */
+    void (*fs)(ascii_serial_com *, char, char, char, const char *data, size_t,
+               void *); /**< called for s messages */
+    void (*fother)(ascii_serial_com *, char, char, char, const char *data,
+                   size_t, void *); /**< called for other messages */
+);
+
+/** \brief ASCII Serial Com Device receive messages
+ *
+ * Initialize ASCII Serial Com device. Actually does the work.
+ *
+ * Receives ASCII Serial Com messages, and hands them over to the
+ * appropriate one of the functions
+ *
+ */
+void ascii_serial_com_device_receive();
