b05b5d4b7ab12f221bd8b3c94405897810954494
Justin Hugon <jhugon@users.noreply.github.com>
2020-08-19 15:17:56 -0500

    arduino_uno_{dummy_register_block,register_pointers}.c: uart Rx Int
      Use an extra input buffer and UART Rx interrupt instead of polling in:
            arduino_uno_dummy_register_block.c
            arduino_uno_register_pointers.c

--- src/avr/arduino_uno_dummy_register_block.c
+++ src/avr/arduino_uno_dummy_register_block.c
@@ -3,10 +3,15 @@
 #include "ascii_serial_com_device.h"
 #include "ascii_serial_com_register_block.h"
 #include "avr/avr_uart.h"
+#include "circular_buffer.h"
 
-#define FOSC 16000000L
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <util/atomic.h>
+
+#define F_CPU 16000000L
 #define BAUD 9600
-#define MYUBRR (FOSC / 16 / BAUD - 1)
+#define MYUBRR (F_CPU / 16 / BAUD - 1)
 
 char dataBuffer[MAXDATALEN];
 #define nRegs 10
@@ -14,6 +19,10 @@ REGTYPE regs[nRegs];
 
 ascii_serial_com_device ascd;
 ascii_serial_com_register_block reg_block;
+
+#define extraInputBuffer_size 64
+uint8_t extraInputBuffer_raw[extraInputBuffer_size];
+circular_buffer_uint8 extraInputBuffer;
 
 CEXCEPTION_T e;
 
@@ -32,12 +41,25 @@ int main(void) {
   circular_buffer_uint8 *asc_out_buf =
       ascii_serial_com_device_get_output_buffer(&ascd);
 
-  USART0_Init(MYUBRR, 0);
+  circular_buffer_init_uint8(&extraInputBuffer, extraInputBuffer_size,
+                             extraInputBuffer_raw);
+
+  USART0_Init(MYUBRR, 1);
+
+  sei();
 
   while (true) {
     Try {
-      if (USART0_can_read_Rx_data) {
-        circular_buffer_push_back_uint8(asc_in_buf, UDR0);
+      // if (USART0_can_read_Rx_data) {
+      //  circular_buffer_push_back_uint8(asc_in_buf, UDR0);
+      //}
+
+      if (!circular_buffer_is_empty_uint8(&extraInputBuffer)) {
+        uint8_t byte;
+        ATOMIC_BLOCK(ATOMIC_FORCEON) {
+          byte = circular_buffer_pop_front_uint8(&extraInputBuffer);
+        }
+        circular_buffer_push_back_uint8(asc_in_buf, byte);
       }
 
       ascii_serial_com_device_receive(&ascd);
@@ -51,4 +73,9 @@ int main(void) {
   }
 
   return 0;
+}
+
+ISR(USART_RX_vect) {
+  char c = UDR0;
+  circular_buffer_push_back_uint8(&extraInputBuffer, c);
 }
--- src/avr/arduino_uno_register_pointers.c
+++ src/avr/arduino_uno_register_pointers.c
@@ -3,11 +3,15 @@
 #include "ascii_serial_com_device.h"
 #include "ascii_serial_com_register_pointers.h"
 #include "avr/avr_uart.h"
-#include "avr/io.h"
+#include "circular_buffer.h"
 
-#define FOSC 16000000L
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <util/atomic.h>
+
+#define F_CPU 16000000L
 #define BAUD 9600
-#define MYUBRR (FOSC / 16 / BAUD - 1)
+#define MYUBRR (F_CPU / 16 / BAUD - 1)
 
 char dataBuffer[MAXDATALEN];
 #define nRegs 3
@@ -21,6 +25,10 @@ REGTYPE masks[nRegs] = {
 
 ascii_serial_com_device ascd;
 ascii_serial_com_register_pointers reg_pointers_state;
+
+#define extraInputBuffer_size 64
+uint8_t extraInputBuffer_raw[extraInputBuffer_size];
+circular_buffer_uint8 extraInputBuffer;
 
 CEXCEPTION_T e;
 
@@ -42,12 +50,25 @@ int main(void) {
   circular_buffer_uint8 *asc_out_buf =
       ascii_serial_com_device_get_output_buffer(&ascd);
 
-  USART0_Init(MYUBRR, 0);
+  circular_buffer_init_uint8(&extraInputBuffer, extraInputBuffer_size,
+                             extraInputBuffer_raw);
+
+  USART0_Init(MYUBRR, 1);
+
+  sei();
 
   while (true) {
     Try {
-      if (USART0_can_read_Rx_data) {
-        circular_buffer_push_back_uint8(asc_in_buf, UDR0);
+      // if (USART0_can_read_Rx_data) {
+      //  circular_buffer_push_back_uint8(asc_in_buf, UDR0);
+      //}
+
+      if (!circular_buffer_is_empty_uint8(&extraInputBuffer)) {
+        uint8_t byte;
+        ATOMIC_BLOCK(ATOMIC_FORCEON) {
+          byte = circular_buffer_pop_front_uint8(&extraInputBuffer);
+        }
+        circular_buffer_push_back_uint8(asc_in_buf, byte);
       }
 
       ascii_serial_com_device_receive(&ascd);
@@ -61,4 +82,9 @@ int main(void) {
   }
 
   return 0;
+}
+
+ISR(USART_RX_vect) {
+  char c = UDR0;
+  circular_buffer_push_back_uint8(&extraInputBuffer, c);
 }
