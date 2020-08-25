3637ef8cfd297c4cf57c571a1f1907395f419222
Justin Hugon <jhugon@users.noreply.github.com>
2020-08-25 09:07:07 -0500

    make-avr.sh: build patched avr-libc for 0 and 1 series

--- tools/make-avr.sh
+++ tools/make-avr.sh
@@ -5,33 +5,62 @@
 
 export PREFIX=`pwd`/avr-install
 
-wget https://mirrors.ocf.berkeley.edu/gnu/binutils/binutils-2.35.tar.xz
-tar xJf binutils-2.35.tar.xz
-cd binutils-*/
-mkdir -p obj-avr
-cd obj-avr
-../configure --prefix=$PREFIX --target=avr --disable-nls >& logConfigure
-make -j8 >& logMake
-make install >& logInstall
-cd ../..
+#wget https://mirrors.ocf.berkeley.edu/gnu/binutils/binutils-2.35.tar.xz
+#tar xJf binutils-2.35.tar.xz
+#cd binutils-*/
+#mkdir -p obj-avr
+#cd obj-avr
+#../configure --prefix=$PREFIX --target=avr --disable-nls >& logConfigure
+#nice make -j8 >& logMake
+#make install >& logInstall
+#cd ../..
 
 export PATH=$PREFIX/bin:$PATH
 
-wget http://mirrors.concertpass.com/gcc/releases/gcc-10.2.0/gcc-10.2.0.tar.xz
-tar xJf gcc-10.2.0.tar.xz
-cd gcc-*/
-./contrib/download_prerequisites
-mkdir -p obj-avr
-cd obj-avr
-../configure --prefix=$PREFIX --target=avr --enable-languages=c,c++ \
-    --disable-nls --disable-libssp --with-dwarf2 >& logConfigure
-make -j8 >& logMake
+#wget http://mirrors.concertpass.com/gcc/releases/gcc-10.2.0/gcc-10.2.0.tar.xz
+#tar xJf gcc-10.2.0.tar.xz
+#cd gcc-*/
+#./contrib/download_prerequisites
+#mkdir -p obj-avr
+#cd obj-avr
+#../configure --prefix=$PREFIX --target=avr --enable-languages=c,c++ \
+#    --disable-nls --disable-libssp --with-dwarf2 >& logConfigure
+#nice make -j8 >& logMake
+#make install >& logInstall
+#cd ../..
+
+######## avr-libc
+
+#git clone https://github.com/jhugon/avr-libc.git
+#cd avr-libc/
+#./bootstrap >& logBootstrap
+#cd ..
+
+### Default
+
+#mkdir -p avr-libc/obj-avr
+#cd avr-libc/obj-avr
+#../configure --prefix=$PREFIX --build=`../config.guess` --host=avr >& logConfigure
+#nice make -j8 >& logMake
+#make install >& logInstall
+#cd ../..
+
+### debug
+export PREFIX=`pwd`/avr-libc-install-debug
+export CFLAGS="-g -Og"
+mkdir -p avr-libc/obj-avr-debug
+cd avr-libc/obj-avr-debug
+../configure --prefix=$PREFIX --build=`../config.guess` --host=avr >& logConfigure
+nice make -j8 >& logMake
 make install >& logInstall
 cd ../..
 
-wget http://download.savannah.gnu.org/releases/avr-libc/avr-libc-2.0.0.tar.bz2
-tar xjf avr-libc-2.0.0.tar.bz2
-cd avr-libc-2.0.0/
-./configure --prefix=$PREFIX --build=`./config.guess` --host=avr >& logConfigure
-make -j8 >& logMake
+### opt
+export PREFIX=`pwd`/avr-libc-install-opt
+export CFLAGS="-Os -flto -Wstrict-aliasing -fstrict-aliasing"
+mkdir -p avr-libc/obj-avr-opt
+cd avr-libc/obj-avr-opt
+../configure --prefix=$PREFIX --build=`../config.guess` --host=avr >& logConfigure
+nice make -j8 >& logMake
 make install >& logInstall
+cd ../..
