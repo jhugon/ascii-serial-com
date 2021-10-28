03e878219a5482d8c3535a2b53febb2ac4c00afc
Justin Hugon <jhugon@users.noreply.github.com>
2021-10-27 21:20:30 -0500

    testLoopback integration tests all working :-)

--- integration_tests/testLoopback.py
+++ integration_tests/testLoopback.py
@@ -200,54 +200,82 @@ class TestASCLoopback(unittest.TestCase):
             b">AFw0123456789.086F\n",
         ]
 
-        with Com_Subproc([self.exe], env=self.env, hideStderr=True) as comSubproc:
-            for intext in intexts:
-                # print("For intext: ", intext)
-                comSubproc.send(intext)
-                tstart = datetime.datetime.now()
-                data = bytearray()
-                while datetime.datetime.now() < tstart + datetime.timedelta(
-                    milliseconds=20
-                ):
-                    data += comSubproc.receive()
-                # print("Got data: '{}'".format(data.decode("UTF-8")), flush=True)
-                self.assertEqual(b"", data)
+        async def run_test(self, intexts):
+            got_to_cancel = False
+            with trio.move_on_after(10) as cancel_scope:
+                async with await trio.open_process(
+                    [self.exe],
+                    stdin=subprocess.PIPE,
+                    stdout=subprocess.PIPE,
+                    # stderr=subprocess.DEVNULL,
+                ) as device:
+                    for intext in intexts:
+                        await device.stdio.send_all(intext)
+                        response = b""
+                        with trio.move_on_after(0.1) as cancel_scope:
+                            while True:
+                                response += await device.stdio.receive_some()
+                        self.assertEqual(response, b"")
+                    got_to_cancel = True
+                    cancel_scope.cancel()
+            self.assertTrue(got_to_cancel)
+
+        trio.run(run_test, self, intexts)
 
     def test_host_device(self):
-        with subprocess.Popen(
-            [self.exe],
-            env=self.env,
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.DEVNULL,
-            bufsize=0,
-            close_fds=True,
-        ) as proc:
-            asc = Ascii_Serial_Com(proc.stdout, proc.stdin, 8)
-            for testCommand in [b"a", b"b", b"c"]:
-                for testData in [b"", b"abcdefg", b"x" * 54]:
-                    asc.send_message(testCommand, testData)
-                    msg = asc.receive_message()
-                    self.assertEqual(testCommand, msg.command)
-                    self.assertEqual(testData, msg.data)
+        async def run_test(self):
+            nRegisterBits = 32
+            got_to_cancel = False
+            with trio.move_on_after(1) as cancel_scope:
+                async with await trio.open_process(
+                    [self.exe],
+                    stdin=subprocess.PIPE,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.DEVNULL,
+                ) as device:
+                    host_w, host_r = breakStapledIntoWriteRead(device.stdio)
+                    send_chan, recv_chan = trio.open_memory_channel(0)
+                    async with trio.open_nursery() as nursery:
+                        asc = Ascii_Serial_Com(nursery, host_r, host_w, nRegisterBits)
+                        asc.forward_all_received_messages_to(send_chan)
+                        for testCommand in [b"a", b"b", b"c"]:
+                            for testData in [b"", b"abcdefg", b"x" * 54]:
+                                await asc.send_message(testCommand, testData)
+                                msg = await recv_chan.receive()
+                                self.assertEqual(msg.command, testCommand)
+                                self.assertEqual(msg.data, testData)
+                        got_to_cancel = True
+                        cancel_scope.cancel()
+            self.assertTrue(got_to_cancel)
+
+        trio.run(run_test, self)
 
     def test_host_device_random(self):
-        with subprocess.Popen(
-            [self.exe],
-            env=self.env,
-            stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.DEVNULL,
-            bufsize=0,
-            close_fds=True,
-        ) as proc:
-            asc = Ascii_Serial_Com(proc.stdout, proc.stdin, 8)
-            for i in range(10000):
-                testCommand = bytes([random.choice(alphabytes)])
-                nData = random.randrange(55)
-                testData = bytes(random.choices(alphanumeric, k=nData))
-                # print(testCommand,testData)
-                asc.send_message(testCommand, testData)
-                msg = asc.receive_message()
-                self.assertEqual(testCommand, msg.command)
-                self.assertEqual(testData, msg.data)
+        async def run_test(self):
+            nRegisterBits = 32
+            got_to_cancel = False
+            with trio.move_on_after(1) as cancel_scope:
+                async with await trio.open_process(
+                    [self.exe],
+                    stdin=subprocess.PIPE,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.DEVNULL,
+                ) as device:
+                    host_w, host_r = breakStapledIntoWriteRead(device.stdio)
+                    send_chan, recv_chan = trio.open_memory_channel(0)
+                    async with trio.open_nursery() as nursery:
+                        asc = Ascii_Serial_Com(nursery, host_r, host_w, nRegisterBits)
+                        asc.forward_all_received_messages_to(send_chan)
+                        for i in range(1000):
+                            testCommand = bytes([random.choice(alphabytes)])
+                            nData = random.randrange(55)
+                            testData = bytes(random.choices(alphanumeric, k=nData))
+                            await asc.send_message(testCommand, testData)
+                            msg = await recv_chan.receive()
+                            self.assertEqual(msg.command, testCommand)
+                            self.assertEqual(msg.data, testData)
+                        got_to_cancel = True
+                        cancel_scope.cancel()
+            self.assertTrue(got_to_cancel)
+
+        trio.run(run_test, self)
