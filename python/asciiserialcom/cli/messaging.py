b6618894ba1e948ec1db89b7ed24c30bfa9edef6
Justin Hugon <jhugon@users.noreply.github.com>
2021-11-23 15:50:08 -0600

    CLI: added stream text graph command
      Completed task in #27
      Now need to see what's messed up in CI

--- python/asciiserialcom/cli/messaging.py
+++ python/asciiserialcom/cli/messaging.py
@@ -2,6 +2,8 @@ from typing import Optional
 from pathlib import Path
 import logging
 from functools import partial
+from shutil import get_terminal_size
+import math
 
 import typer
 import trio
@@ -196,6 +198,109 @@ async def run_stream(
                         partial(trio.sleep, timeout), stop_event.wait
                     )
                     await host.stop_streaming()
+
+
+async def run_stream_text_graph(
+    minimum_value, maximum_value, timeout, stop_messages, stop_bytes, stop_lines,
+):
+    fin_name = SERIAL
+    fout_name = SERIAL
+    if SERIAL_SEND:
+        fout_name = SERIAL_SEND
+    logging.debug(f"fin_name: {fin_name}")
+    logging.debug(f"fout_name: {fout_name}")
+    logging.debug(f"timeout: {timeout}")
+    send_ch, recv_ch = trio.open_memory_channel(0)
+    if timeout is None:
+        timeout = float("inf")
+    timeout_cancel = timeout + 0.5
+    with trio.move_on_after(timeout_cancel) as cancel_scope:
+        async with trio.open_nursery() as nursery:
+            logging.debug(f"About to open files")
+            async with await trio.open_file(fin_name, "br") as fin:
+                async with await trio.open_file(fout_name, "bw") as fout:
+                    if fin_name.is_char_device():
+                        setup_tty(fin.wrapped, BAUD)
+                    logging.debug(f"Files open!")
+                    host = Host(nursery, fin, fout, 8, ignoreErrors=True)
+                    stop_event = trio.Event()
+                    nursery.start_soon(
+                        forward_received_messages_to_text_graph,
+                        recv_ch,
+                        minimum_value,
+                        maximum_value,
+                        stop_messages,
+                        stop_bytes,
+                        stop_lines,
+                        stop_event,
+                    )
+                    host.forward_received_s_messages_to(send_ch)
+                    await host.start_streaming()
+                    await trio_util.wait_any(
+                        partial(trio.sleep, timeout), stop_event.wait
+                    )
+                    await host.stop_streaming()
+
+
+async def forward_received_messages_to_text_graph(
+    ch, minimum_value, maximum_value, stop_messages, stop_bytes, stop_lines, stop_event,
+):
+    logging.debug("Started")
+    totalMessages = 0
+    totalBytes = 0
+    totalLines = 0
+    totalMissedMessages = 0
+    value_span = maximum_value - minimum_value
+    minimum_value_str_width = len(str(minimum_value))
+    maximum_value_str_width = len(str(maximum_value))
+    term_width = get_terminal_size()[0]
+    space_width = term_width - minimum_value_str_width - maximum_value_str_width
+    typer.echo("=" * term_width)
+    typer.echo(str(minimum_value) + " " * space_width + str(maximum_value))
+    typer.echo("-" * term_width)
+    try:
+        while True:
+            try:
+                nMissed, payload = await ch.receive()
+            except trio.EndOfChannel:
+                break
+            else:
+                out_text = ""
+                totalMessages += 1
+                totalBytes += len(payload)
+                msg_text = payload.decode("ascii", "replace")
+                logging.debug(f"term_width: {term_width} value_span: {value_span}")
+                for x in msg_text.split(" "):
+                    totalLines += 1
+                    msg_value = int(msg_text, 16)
+                    pos = (msg_value - minimum_value) / value_span * term_width
+                    pos_int = int(math.ceil(pos))
+                    pos_int = max(pos_int, 0)
+                    pos_int = min(pos_int, term_width - 1)
+                    logging.debug(f"value: {msg_value} pos: {pos} pos_int: {pos_int}")
+                    out_text += " " * pos_int + "o\n"
+                typer.echo(out_text, nl=False)
+                if stop_messages and totalMessages >= stop_messages:
+                    stop_event.set()
+                    break
+                elif stop_bytes and totalBytes >= stop_bytes:
+                    stop_event.set()
+                    break
+                elif stop_lines and totalLines >= stop_lines:
+                    stop_event.set()
+                    break
+            finally:
+                if totalMissedMessages > 0:
+                    logging.warning(
+                        f"Receiver missed a total of {totalMissedMessages} messages"
+                    )
+        term_width = get_terminal_size()[0]
+        space_width = term_width - minimum_value_str_width - maximum_value_str_width
+        typer.echo("-" * term_width)
+        typer.echo(str(minimum_value) + " " * space_width + str(maximum_value))
+        typer.echo("=" * term_width)
+    except Exception as e:
+        raise e
 
 
 app = typer.Typer()
@@ -421,6 +526,59 @@ def stream(
             stop_datasep,
             split_seperators_newlines,
             decode_hex_to_dec,
+        )  # ,instruments=[Tracer()])
+    except Exception as e:
+        typer.echo(f"Error: unhandled exception: {type(e)}: {e}", err=True)
+
+
+@app.command()
+def stream_text_graph(
+    minimum: int = typer.Argument(
+        ..., help="Minimum expected value for setting graph limits.", min=0
+    ),
+    maximum: int = typer.Argument(
+        ..., help="Maximum expected value for setting graph limits.", min=1
+    ),
+    stop_seconds: Optional[float] = typer.Option(
+        None, "--stop-seconds", "-t", help="Stop after this many seconds"
+    ),
+    stop_messages: Optional[int] = typer.Option(
+        None,
+        "--stop-messages",
+        "-m",
+        help="Stop after this many messages have been received",
+    ),
+    stop_bytes: Optional[int] = typer.Option(
+        None, "--stop-bytes", "-b", help="Stop after this many bytes have been received"
+    ),
+    stop_lines: Optional[int] = typer.Option(
+        None,
+        "--stop-lines",
+        "-l",
+        help="Stop after this many lines (could differ from --stop-messages due to data seperators within messages)",
+    ),
+) -> None:
+    """
+    Converts each message (or part of one due to seperators) into an int and then plots them in the terminal in real time.
+    Each sample (or part of one) gets a new line and a symbol is placed the appropriate position between MINIMUM and MAXIMUM.
+
+    With all the stop arguments and hitting Ctrl-C: whichever happens first will stop streaming.
+    """
+    if SERIAL_SEND:
+        typer.echo(
+            f"Receive streaming data with send device {SERIAL_SEND} and read device {SERIAL}"
+        )
+    else:
+        typer.echo(f"Receive streaming data with device {SERIAL}")
+    try:
+        trio.run(
+            run_stream_text_graph,
+            minimum,
+            maximum,
+            stop_seconds,
+            stop_messages,
+            stop_bytes,
+            stop_lines,
         )  # ,instruments=[Tracer()])
     except Exception as e:
         typer.echo(f"Error: unhandled exception: {type(e)}: {e}", err=True)
