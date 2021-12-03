67422b7f4b1c01230c4bef095ca5716f3f78a8de
Justin Hugon <jhugon@users.noreply.github.com>
2021-12-03 10:24:57 -0600

    stm32f091nucleo64_asc_loopback tested and working
      Seemed to be really easy :-)

--- nucleo-f091rc_integration_tests/test_loopback.py
+++ nucleo-f091rc_integration_tests/test_loopback.py
@@ -190,3 +190,84 @@ class TestCharLoopback(unittest.TestCase):
             self.assertTrue(data_checker_finished.is_set())
 
         trio.run(run_test, self)
+
+
+class TestASCLoopback(unittest.TestCase):
+    """
+        Requires firmware:
+
+        openocd -f /usr/share/openocd/scripts/board/st_nucleo_f0.cfg -c "program build/cortex-m0_gcc_debug/stm32f091nucleo64_asc_loopback.elf verify reset
+
+    """
+
+    def setUp(self):
+        self.baud = 9600
+        self.dev_path = "/dev/ttyACM0"
+        # self.dev_path = "/dev/ttyACM1"
+
+        logging.basicConfig(
+            # filename="test_asciiSerialCom.log",
+            level=logging.INFO,
+            # level=logging.DEBUG,
+            # level=logging.WARNING,
+            format="%(levelname)s L%(lineno)d %(funcName)s: %(message)s",
+        )
+        setup_tty(self.dev_path, self.baud)
+
+    def test_host_device(self):
+        """
+        Send ASC messages to device and read them back
+        """
+        nRegisterBits = 32
+        messages = [
+            ASC_Message(b"0", b"0", b"r", b"00"),
+            ASC_Message(b"0", b"0", b"r", b"04FF"),
+            ASC_Message(b"0", b"0", b"w", b"04FF,00000000"),
+            ASC_Message(b"0", b"0", b"w", b"04FF,FFFFFFFF"),
+            ASC_Message(b"0", b"0", b"s", b"04FF FFFFFFFF"),
+        ] * 10
+
+        async def data_checker(
+            self, chan, data_checker_finished, task_status=trio.TASK_STATUS_IGNORED
+        ):
+            logging.debug("Starting data_checker")
+            task_status.started()
+            try:
+                for iMessage in range(len(messages)):
+                    logging.debug(f"Trying to receive message {iMessage}")
+                    msg = await chan.receive()
+                    logging.debug(f"message {iMessage} received: {msg}")
+                    self.assertEqual(msg, messages[iMessage])
+                data_checker_finished.set()
+            except trio.EndOfChannel as e:
+                logging.error("Receive channel closed unexpectedly")
+
+        async def run_test(self):
+            logging.info("Starting run_test")
+            send_chan, recv_chan = trio.open_memory_channel(100)
+            data_checker_finished = trio.Event()
+            with trio.move_on_after(30) as cancel_scope:
+                logging.info("About to open file...")
+                async with await trio.open_file(self.dev_path, "br") as portr:
+                    async with await trio.open_file(self.dev_path, "bw") as portw:
+                        termios.tcflush(portr.wrapped, termios.TCIFLUSH)
+                        logging.debug("TTY setup and flushed")
+                        async with trio.open_nursery() as nursery:
+                            logging.debug("About to startup host")
+                            host = Host(nursery, portr, portw, nRegisterBits)
+                            logging.debug("Host started")
+                            host.forward_all_received_messages_to(send_chan)
+                            logging.debug("messages now forwarded to send_chan")
+                            await nursery.start(
+                                data_checker, self, recv_chan, data_checker_finished
+                            )
+                            logging.debug("data_checker started")
+                            for msg in messages:
+                                await host.send_message(msg.command, msg.data)
+                                await trio.sleep(0.05)
+                            await data_checker_finished.wait()
+                            logging.debug("data_checker_finished")
+                            nursery.cancel_scope.cancel()
+            self.assertTrue(data_checker_finished.is_set())
+
+        trio.run(run_test, self)
