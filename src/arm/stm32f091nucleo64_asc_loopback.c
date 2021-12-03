f9f5336d07de36b4c5468546b4585008478af2bd
Justin Hugon <jhugon@users.noreply.github.com>
2021-12-03 09:56:10 -0600

    created stm32f091nucleo64_asc_loopback
      copied from char loopback and changed to use rx interrupt

---
+++ src/arm/stm32f091nucleo64_asc_loopback.c
@@ -0,0 +1,54 @@
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/usart.h>
+
+// USART2 should be connected through USB
+// USART2 TX: PA2 AF1
+// USART2 RX: PA3 AF1
+static void usart_setup(void) {
+  rcc_periph_clock_enable(RCC_USART2);
+  rcc_periph_clock_enable(RCC_GPIOA);
+
+  gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
+  gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO3);
+
+  gpio_set_af(GPIOA, GPIO_AF1, GPIO2);
+  gpio_set_af(GPIOA, GPIO_AF1, GPIO3);
+
+  nvic_enable_irq(NVIC_USART2_IRQ);
+
+  usart_set_baudrate(USART2, 9600);
+  usart_set_databits(USART2, 8);
+  usart_set_parity(USART2, USART_PARITY_NONE);
+  usart_set_stopbits(USART2, USART_CR2_STOPBITS_1);
+  usart_set_mode(USART2, USART_MODE_TX_RX);
+  usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+
+  usart_enable_rx_interrupt(USART2);
+
+  usart_enable(USART2);
+}
+
+uint8_t tmp_byte = 0;
+
+int main(void) {
+
+  usart_setup();
+
+  while (1) {
+    if (tmp_byte && (USART_ISR(USART2) & USART_ISR_TXE)) {
+      usart_send(USART2, tmp_byte);
+      tmp_byte = 0;
+    }
+  }
+
+  return 0;
+}
+
+void usart2_isr(void) {
+  if (((USART_CR1(USART2) & USART_CR1_RXNEIE) != 0) &&
+      ((USART_ISR(USART2) & USART_ISR_RXNE) != 0)) {
+    tmp_byte = usart_recv(USART2) & 0xFF;
+  }
+}
