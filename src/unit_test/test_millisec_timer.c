7965888e5178bafa27be8195ae075bf68dfe5c45
Justin Hugon <jhugon@users.noreply.github.com>
2021-12-04 20:06:36 -0600

    millisec_timer tests: added wrap once tests

--- src/unit_test/test_millisec_timer.c
+++ src/unit_test/test_millisec_timer.c
@@ -112,7 +112,109 @@ void test_millisec_timer_not_wrap_once(void) {
   TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, MAXVAL));
 }
 
-void test_millisec_timer_do_wrap_once(void) {}
+void test_millisec_timer_do_wrap_once(void) {
+  millisec_timer timer;
+
+  ////////////////////////////////////////////////////
+
+  millisec_timer_set_rel(&timer, 1, MAXVAL);
+  TEST_ASSERT_EQUAL_UINT32_MESSAGE(
+      1, timer.set_time,
+      "Checking the test is what it should be, not an error with millisec "
+      "timer but with the test");
+  TEST_ASSERT_EQUAL_UINT32_MESSAGE(
+      0, timer.expire_time,
+      "Checking the test is what it should be, not an error with millisec "
+      "timer but with the test");
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 1));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 2));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, MAXVAL));
+  TEST_ASSERT_TRUE(millisec_timer_is_expired(&timer, 0));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 0));
+
+  ////////////////////////////////////////////////////
+
+  millisec_timer_set_rel(&timer, 2, MAXVAL);
+  TEST_ASSERT_EQUAL_UINT32_MESSAGE(
+      2, timer.set_time,
+      "Checking the test is what it should be, not an error with millisec "
+      "timer but with the test");
+  TEST_ASSERT_EQUAL_UINT32_MESSAGE(
+      1, timer.expire_time,
+      "Checking the test is what it should be, not an error with millisec "
+      "timer but with the test");
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 2));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 3));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, MAXVAL));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 0));
+  TEST_ASSERT_TRUE(millisec_timer_is_expired(&timer, 1));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 1));
+
+  ////////////////////////////////////////////////////
+
+  millisec_timer_set_rel(&timer, MAXVAL - 1, 2);
+  TEST_ASSERT_EQUAL_UINT32_MESSAGE(
+      MAXVAL - 1, timer.set_time,
+      "Checking the test is what it should be, not an error with millisec "
+      "timer but with the test");
+  TEST_ASSERT_EQUAL_UINT32_MESSAGE(
+      0, timer.expire_time,
+      "Checking the test is what it should be, not an error with millisec "
+      "timer but with the test");
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, MAXVAL - 1));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, MAXVAL));
+  TEST_ASSERT_TRUE(millisec_timer_is_expired(&timer, 0));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 0));
+  millisec_timer_set_rel(&timer, MAXVAL - 1, 2);
+  TEST_ASSERT_TRUE(millisec_timer_is_expired(&timer, 1));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 1));
+  millisec_timer_set_rel(&timer, MAXVAL - 1, 2);
+  TEST_ASSERT_TRUE(millisec_timer_is_expired(&timer, MAXVAL - 2));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, MAXVAL - 2));
+
+  ////////////////////////////////////////////////////
+
+  millisec_timer_set_rel(&timer, MAXVAL, 1);
+  TEST_ASSERT_EQUAL_UINT32_MESSAGE(
+      MAXVAL, timer.set_time,
+      "Checking the test is what it should be, not an error with millisec "
+      "timer but with the test");
+  TEST_ASSERT_EQUAL_UINT32_MESSAGE(
+      0, timer.expire_time,
+      "Checking the test is what it should be, not an error with millisec "
+      "timer but with the test");
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, MAXVAL));
+  TEST_ASSERT_TRUE(millisec_timer_is_expired(&timer, 0));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 0));
+  millisec_timer_set_rel(&timer, MAXVAL, 1);
+  TEST_ASSERT_TRUE(millisec_timer_is_expired(&timer, 1));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 1));
+  millisec_timer_set_rel(&timer, MAXVAL, 1);
+  TEST_ASSERT_TRUE(millisec_timer_is_expired(&timer, MAXVAL - 1));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, MAXVAL - 1));
+
+  ////////////////////////////////////////////////////
+
+  millisec_timer_set_rel(&timer, MAXVAL, 2);
+  TEST_ASSERT_EQUAL_UINT32_MESSAGE(
+      MAXVAL, timer.set_time,
+      "Checking the test is what it should be, not an error with millisec "
+      "timer but with the test");
+  TEST_ASSERT_EQUAL_UINT32_MESSAGE(
+      1, timer.expire_time,
+      "Checking the test is what it should be, not an error with millisec "
+      "timer but with the test");
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, MAXVAL));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 0));
+  TEST_ASSERT_TRUE(millisec_timer_is_expired(&timer, 1));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 1));
+  millisec_timer_set_rel(&timer, MAXVAL, 2);
+  TEST_ASSERT_TRUE(millisec_timer_is_expired(&timer, 2));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, 2));
+  millisec_timer_set_rel(&timer, MAXVAL, 2);
+  TEST_ASSERT_TRUE(millisec_timer_is_expired(&timer, MAXVAL - 1));
+  TEST_ASSERT_FALSE(millisec_timer_is_expired(&timer, MAXVAL - 1));
+}
 
 void test_millisec_timer_not_wrap_repeat(void) {}
 
